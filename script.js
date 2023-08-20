9619d41c67996911b9fb2b5668c79c86be0ab111
raiyanrimon <raiyan1615@sttudent.nstu.edu.bd>
2023-08-21 02:45:17 +0600

    final touch

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let total = 0;
-function handleCLickButton(target){
+function handleCLickButton(target) {
     const cardCalculationEntry = document.getElementById('cart-calculation-entry');
     const count = cardCalculationEntry.childElementCount;
     const itemName = target.childNodes[3].childNodes[3].innerText;
@@ -10,47 +10,42 @@ function handleCLickButton(target){
     p.innerHTML = `${count + 1}. ${itemName} ${price} TK`;
     cardCalculationEntry.appendChild(p);
 
-    
-    total = parseFloat(total) + parseFloat (price);
-    totalPrice = total.toFixed(2);
-  document.getElementById('total-price').innerText = totalPrice;
 
-   if(totalPrice > 0){
-    const purchaseButton = document.getElementById('purchase');
-    purchaseButton.disabled = false;
-   }
-   if (totalPrice >= 200){
-    const couponBtn = document.getElementById('coupon');
-    couponBtn.disabled = false;
-    
-   }
-   
+    total = parseFloat(total) + parseFloat(price);
+    totalPrice = total.toFixed(2);
+    document.getElementById('total-price').innerText = totalPrice;
+
+    if (totalPrice > 0) {
+        const purchaseButton = document.getElementById('purchase');
+        purchaseButton.disabled = false;
+    }
+    if (totalPrice >= 200) {
+        const couponBtn = document.getElementById('coupon');
+        couponBtn.disabled = false;
+    }
 }
 
-function discountClick(){
-    if(inputField = 'SELL20'){
+function discountClick() {
+    if (inputField = 'SELL20') {
         const inputField = document.getElementById('input');
-    const discountAmountText = document.getElementById('discount');
-    const finalTotalText = document.getElementById('last-total');
-    const discountPercentage = 20;
+        const discountAmountText = document.getElementById('discount');
+        const finalTotalText = document.getElementById('last-total');
+        const discountPercentage = 20;
         const discountAmount = (totalPrice * discountPercentage) / 100;
         discountAmountText.innerText = parseFloat(discountAmount).toFixed(2);
         const finalTotal = (totalPrice - discountAmount);
         finalTotalText.innerText = parseFloat(finalTotal).toFixed(2);
-       
     }
 }
 
-function goHome(){
-    const cardCalculationEntry = document.getElementById('cart-calculation-entry');
-    cardCalculationEntry.innerHTML = '';
-    const inputField = document.getElementById('input');
-    inputField.value = '';
-    const discountAmountText = document.getElementById('discount');
-    discountAmountText.innerText = '00.00';
-    const finalTotalText = document.getElementById('last-total');
-    finalTotalText.innerText = '00.00';
+function goHome() {
+    document.getElementById('cart-calculation-entry').innerHTML = '';
+    document.getElementById('input').value = '';
+    document.getElementById('discount').innerText = '00.00';
+    document.getElementById('last-total').innerText = '00.00';
     document.getElementById('total-price').innerText = '00.00';
+    document.getElementById('coupon').disabled = true;
+    document.getElementById('purchase').disabled = true;
 }
 
 
@@ -82,7 +77,7 @@ function goHome(){
 
 
 
-function addToCartCalculationEntry(){
+function addToCartCalculationEntry() {
     const cardCalculationEntry = document.getElementById('cart-calculation-entry');
     const count = cardCalculationEntry.childElementCount;;
     const li = document.createElement('li');
